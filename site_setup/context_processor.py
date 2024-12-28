68040e6002d1c62c3680c91d01ac69712d47fe48
styven <styvenn16@gmail.com>
2024-12-28 09:08:01 -0300

    Usando Context Processors para injetar valores em todos os templates do Django

---
+++ site_setup/context_processor.py
@@ -0,0 +1,13 @@
+from site_setup.models import SiteSetup
+
+def context_processor_example(request):
+    return {
+        'example':'context'
+    }
+
+
+def site_setup(request):
+    data = SiteSetup.objects.order_by('-id').first()
+    return {
+        'site_setup': data
+}
