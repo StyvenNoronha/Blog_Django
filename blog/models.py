1a5ab1d095f9b596d382e4146cc7ec035d51c3d4
styven <styvenn16@gmail.com>
2024-12-29 07:27:23 -0300

    Criando os models Tag e Category + SlugField, slug e slugify no Django

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from utils.rands import slugify_new
 # Create your models here.
+class Tag(models.Model):
+    class Meta:
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
+
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, default=None, null=True, blank=True, max_length=100)    
+
+
+    def save(self, *args,**kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.name)
+            return super().save(*args,**kwargs)
