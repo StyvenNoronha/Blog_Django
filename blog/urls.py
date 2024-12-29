b1778ca0fc08caa07c4db6b08d08d8f82eb9fad6
styven <styvenn16@gmail.com>
2024-12-29 15:18:11 -0300

    Iniciando a parte interna do Post (vários detalhes interessantes)

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from blog.views import index, page, post
+from blog.views import index, page, post, author, category
 
 app_name = "blog"
 urlpatterns = [
    path('', index, name='index'),
-   path('page/', page, name='page'),
+   path('page/<slug:slug>', page, name='page'),
    path('post/<slug:slug>', post, name='post'),
+   path('author/<int:author_id>', author, name='author'),
+   path('category/<slug:slug>', category, name='category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,11 +12,36 @@ def index(request):
 
 
 
-def page(request):
+
+def author(request, author_id):
+    posts = Post.objects.get_published().filter(created_by__pk=author_id)
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request,'blog/pages/index.html',{'page_obj':page_obj})
+
+
+
+
+
+
+def category(request, slug):
+    posts = Post.objects.get_published().filter(category__slug=slug)
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request,'blog/pages/index.html',{'page_obj':page_obj})
+
+
+
+
+def page(request, slug):
     return render(request,'blog/pages/page.html')
 
 
 
 
-def post(request):
-    return render(request,'blog/pages/post.html')
+def post(request, slug):
+    post = Post.objects.get_published().filter(slug=slug).first()
+
+    return render(request,'blog/pages/post.html',{'post':post})
