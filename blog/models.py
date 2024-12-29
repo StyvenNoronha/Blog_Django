039b2670e46e3ef07df42542c96cb823628c68da
styven <styvenn16@gmail.com>
2024-12-29 09:28:25 -0300

    Criando o model Post, related_name em ForeignKey e outros detalhes

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from utils.rands import slugify_new
+from django.contrib.auth.models import User
 # Create your models here.
 class Tag(models.Model):
     class Meta:
@@ -10,10 +10,6 @@ class Tag(models.Model):
     slug = models.SlugField(unique=True, default=None, null=True, blank=True, max_length=100)    
 
 
-    def save(self, *args,**kwargs):
-        if not self.slug:
-            self.slug = slugify_new(self.name)
-            return super().save(*args,**kwargs)
         
     def __str__(self):
         return self.name  
@@ -28,11 +24,7 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True, default=None, null=True, blank=True, max_length=100)    
 
-
-    def save(self, *args,**kwargs):
-        if not self.slug:
-            self.slug = slugify_new(self.name)   
-            return super().save(*args,**kwargs)     
+ 
 
 
     def __str__(self):
@@ -42,18 +34,38 @@ class Category(models.Model):
 
 
 class Page(models.Model):
-    title = models.CharField(max_length=65,)
+    title = models.CharField(max_length=65)
     slug = models.SlugField(unique=True, default=None, null=True, blank=True, max_length=100)   
 
     is_published = models.BooleanField(default=False)
     content = models.TextField()  
 
-    def save(self, *args,**kwargs):
-        if not self.slug:
-            self.slug = slugify_new(self.title)   
-            return super().save(*args,**kwargs)   
+
 
 
     def __str__(self):
         return self.title
-              
+    
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    title  = models.CharField(max_length=65)
+    slug = models.SlugField(unique=True, default=None, null=True, blank=True, max_length=100)  
+    excerpt = models.CharField(max_length=150)
+    is_published = models.BooleanField(default=False)
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/%Y/%m', blank=True, default="")
+    cover_in_post_content = models.BooleanField(default=True)  
+    created_at = models.DateField(auto_now_add=True)     
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name='post_created_by' )   
+    updated_at = models.DateField(auto_now=True) 
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name='post_updated_by' )  
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)   
+    tags = models.ManyToManyField(Tag, blank=True, default="")    
+
+
+    def __str__(self):
+        return self.title
