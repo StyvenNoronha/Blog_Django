c3ebef945ff8ff399f82d22d1e9eb3ca9cb4c307
styven <styvenn16@gmail.com>
2024-12-29 08:26:54 -0300

    Criando o model e admin Page (models.Model e admin.ModelAdmin do Django)

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,46 @@ class Tag(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.name)
             return super().save(*args,**kwargs)
+        
+    def __str__(self):
+        return self.name  
+        
+        
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, default=None, null=True, blank=True, max_length=100)    
+
+
+    def save(self, *args,**kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.name)   
+            return super().save(*args,**kwargs)     
+
+
+    def __str__(self):
+        return self.name
+          
+
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(unique=True, default=None, null=True, blank=True, max_length=100)   
+
+    is_published = models.BooleanField(default=False)
+    content = models.TextField()  
+
+    def save(self, *args,**kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title)   
+            return super().save(*args,**kwargs)   
+
+
+    def __str__(self):
+        return self.title
+              
